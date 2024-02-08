665ca8d48854038d23300097fb8cfb7835e256ea
vany <emirbeganovic03@gmail.com>
2024-02-08 23:47:19 +0100

    gave the right type to session in login

--- src/login/Login.ts
+++ src/login/Login.ts
@@ -1,7 +1,7 @@
 import { Client } from "../client/Client";
-
+import { session } from "../session/Session";
 class login {
-    constructor(session: ) {
+    constructor(session: session) {
         
     }
 }
